7446819ba4bba32447b5cb24f619096cceb78853
mdoprog <oliveira.demarco@gmail.com>
2021-05-20 03:37:36 -0300

    Fixed error that prevented snake growth

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+let canvas = document.getElementById("snake"); //cria elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //cria cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 let box = 32;
 let snake = [];
 snake[0] = {
@@ -15,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
 function criarCobrinha() {
@@ -30,13 +30,15 @@ function drawFood() {
     context.fillRect(food.x, food.y, box, box);
 }
 
+
+//quando um evento acontece, detecta e chama uma função
 document.addEventListener('keydown', update);
 
-function update (event) {
-    if (event.keyCode == 37 && direction != "right") direction = "left";
-    if (event.keyCode == 38 && direction != "down") direction = "up";
-    if (event.keyCode == 39 && direction != "left") direction = "right";
-    if (event.keyCode == 40 && direction != "up") direction = "down";
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function iniciarJogo() {
@@ -65,21 +67,19 @@ function iniciarJogo() {
     if (direction == "down") snakeY += box;
 
     if (snakeX != food.x || snakeY != food.y) {
-        snake.pop();
+        snake.pop();  // tira o último elemento da lista
     } 
     else {
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-    snake.pop();
-
     let newHead = {
         x: snakeX, 
         y: snakeY
     }
 
-    snake.unshift(newHead);
+    snake.unshift(newHead); //método unshift adiciona o primeiro quadradinho da cobrinha
 
 }
 
